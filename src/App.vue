69ce7f2081f71605a8eb450db7bd14f6bae45cb3
Tuan Duong <tuanduong15@gmail.com>
2016-04-11 15:57:53 -0700

    Add lots of stuff

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,134 @@
 <template>
   <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
+    <div class="animated fadeIn" v-if="currentImage === 'background'">
+      <img class="responsive-img" src="./assets/background.jpg" alt="">      
+    </div>
+    <div class="animated fadeIn" v-if="currentImage === 'folded'">
+      <img class="responsive-img" src="./assets/folded.jpg" alt="">      
+    </div>
+    <div class="animated fadeIn" v-if="currentImage === 'spread'">
+      <img class="responsive-img" src="./assets/spread.jpg" alt="">      
+    </div>
+    <div class="animated fadeIn" v-if="currentImage === 'front'">
+      <img class="responsive-img" src="./assets/front.jpg" alt="">      
+    </div>
+    <div class="animated fadeIn" v-if="currentImage === 'back'">
+      <img class="responsive-img" src="./assets/back.jpg" alt="">      
+    </div>
+    <div class="animated fadeIn" v-if="currentImage === 'side'">
+      <img class="responsive-img" src="./assets/side.jpg" alt="">      
+    </div>
+    <div class="animated fadeIn" v-if="currentImage === 'top'">
+      <img class="responsive-img" src="./assets/top.jpg" alt="">      
+    </div>
+    <div class="animated fadeIn" v-if="currentImage === 'full'">
+      <img class="responsive-img" src="./assets/full.jpg" alt="">      
+    </div>
+    <div class="row z-depth-1 hide-on-small-only">
+      <div @click="handleImageClick('folded')" id="image-1" class="col l3  m3 s6"></div>
+      <div @click="handleImageClick('spread')" id="image-2" class="col l3  m3 s6"></div>
+      <div @click="handleImageClick('front')" id="image-3" class="col l3  m3 s6"></div>
+      <div @click="handleImageClick('back')" id="image-4" class="col l3  m3 s6"></div>
+      <div @click="handleImageClick('side')" id="image-5" class="col l3  m3 s6"></div>
+      <div @click="handleImageClick('top')" id="image-6" class="col l3  m3 s6"></div>
+      <div @click="handleImageClick('full')" id="image-7" class="col l3  m3 s6"></div>
+      <div @click="handleImageClick('background')" id="image-8" class="col l3  m3 s6"></div>
+    </div>
+  </div>
+
+  <div class="row z-depth-1 hide-on-med-and-up">
+      <div @click="handleImageClick('folded')" id="image-1" class="col s3 image-small"></div>
+      <div @click="handleImageClick('spread')" id="image-2" class="col s3 image-small"></div>
+      <div @click="handleImageClick('front')" id="image-3" class="col s3 image-small"></div>
+      <div @click="handleImageClick('back')" id="image-4" class="col s3 image-small"></div>
+      <div @click="handleImageClick('side')" id="image-5" class="col s3 image-small"></div>
+      <div @click="handleImageClick('top')" id="image-6" class="col s3 image-small"></div>
+      <div @click="handleImageClick('full')" id="image-7" class="col s3 image-small"></div>
+      <div @click="handleImageClick('background')" id="image-8" class="col s3 image-small"></div>
+    </div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
-export default {
-  components: {
-    Hello
+  export default {
+    data() {
+      return {
+        currentImage: 'background'
+      }
+    },
+    methods: {
+      handleImageClick(image){
+        this.currentImage = image
+      }
+    }
   }
-}
+
 </script>
 
+
 <style>
-html {
-  height: 100%;
+  .animated {
+    animation-duration: 0.4s;
+    -webkit-animation-duration: 0.4s;
+    -moz-animation-duration: 0.4s;
+  }
+
+  html {
+    height: 100%;
+    background-color: white;,
+    font-family: GillSans, Calibri, Trebuchet, sans-serif;
+  }
+
+  .image-small {
+    max-height: 100px;
+  }
+
+  #image-1 {
+    height: 200px;
+    background-size: cover;
+    background-image: url('./assets/folded.jpg')
+  }
+
+  #image-2 {
+    height: 200px;
+    background-size: cover;
+    background-image: url('./assets/spread.jpg')
+  }
+
+  #image-3 {
+    height: 200px;
+    background-size: cover;
+    background-image: url('./assets/front.jpg')
+  }
+
+  #image-4 {
+    height: 200px;
+    background-size: cover;
+    background-image: url('./assets/back.jpg')
+  }
+
+  #image-5 {
+    height: 200px;
+    background-size: cover;
+    background-image: url('./assets/side.jpg')
+  }
+
+  #image-6 {
+    height: 200px;
+    background-size: cover;
+    background-image: url('./assets/top.jpg')
+  }
+
+  #image-7 {
+    height: 200px;
+    background-size: cover;
+    background-image: url('./assets/full.jpg')
+  }
+
+  #image-8 {
+    height: 200px;
+    background-size: cover;
+    background-image: url('./assets/background.jpg')
 }
 
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
-}
 </style>
---
+++ src/components/CurrentImage.vue
@@ -0,0 +1,12 @@
+<template>
+	<img src="./assets/background.jpg" class="responsive-image">
+</template>
+
+<script>
+	export default {
+		ready() {
+			console.log(this.imageSrc)
+		},
+		props: ['imageSrc']
+	}
+</script>
